4f0113171adc895f8487506db5c629d0f013afa1
Cepexx <82107568+Cepexx@users.noreply.github.com>
2021-10-08 09:11:24 +0200

    Nombre de nuevo

--- Prueba.java
+++ Prueba.java
@@ -4,6 +4,7 @@ public class Prueba {
 
 		System.out.println("Jose Antonio Cepedano Lobejón");
 		System.out.println("León, 2000");
-		System.out.println("Villanueva Del Árbol, Windows 10 pro student");
+		System.out.println("Villanueva Del Arbol, Windows 10 pro student");
+		System.out.println("Jose Antonio Cepedano Lobejón otra vez");
 	}
 }
